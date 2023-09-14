6355dcac01427c5814685643737b81c60e1961d7
CollierTR <80648892+CollierTR@users.noreply.github.com>
2023-09-13 22:49:47 -0500

    Twitter clone launch!

---
+++ data.js
@@ -0,0 +1,118 @@
+export const tweetsData = [   
+    {
+        handle: `@BJJ troll`,
+        profilePic: `images/bjj.jpeg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `BJJ is the only martial art that works in the street!`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@Bruce Lee`,
+        profilePic: `images/Bruce-Lee.jpg`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `I fear not the man who has practiced 10,000 kicks once, but I fear the man who has practiced one kick 10,000 times.`,
+        replies: [
+                  {
+                handle: `@Jackie Chan`,
+                profilePic: `images/jackie-chan.jpg`,
+                tweetText: `Let's do a movie together!`,
+            },
+                  {
+                handle: `@ChuckNorris`,
+                profilePic: `images/chucknorris.jpeg`,
+                tweetText: `I invented kicks!`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@Ninja`,
+        profilePic: `images/ninja.jpeg`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `What's your favorite weapon?`,
+        replies: [
+            {
+                handle: `@Funakoshi`,
+                profilePic: `images/Funakoshi.jpeg`,
+                tweetText: `I like all of the the kubodo weapons.`,
+            },
+            {
+                handle: `@bJJ troll`,
+                profilePic: `images/bjj.jpeg`,
+                tweetText: `You don't need weapons when you know BJJ!`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+    {
+        handle: "@SenseiMiyagi",
+        profilePic: "images/myagi.jpg",
+        likes: 8900,
+        retweets: 321,
+        tweetText: "Wax on, wax off. The path to mastery begins with discipline and patience. 🕊️ #KarateKid",
+        replies: [
+            {
+                handle: "@JohnnyLawrence",
+                profilePic: "images/john.jpeg",
+                tweetText: "Miyagi-Do, Cobra Kai... let's settle this on the mat! 🥊 #KarateRivalry"
+            }
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: "9f8b1e7d-3e7c-45a1-a2d5-8622fbcf5c2f"
+    },
+    {
+    handle: "@Ninja",
+    profilePic: "images/ninja.jpeg",
+    likes: 4200,
+    retweets: 152,
+    tweetText: "Training in the shadows for the ultimate challenge. Who's with me? 💥🌙 #NinjaLife",
+    replies: [
+        {
+        handle: "@SenseiMiyagi",
+        profilePic: "images/myagi.jpg",
+        tweetText: "I have some chores you can do instead!"
+        }
+    ],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: "6a12e8fd-4d9b-4c21-bf8f-96f5e7f831d1"
+    },
+        {
+            handle: "@Funakoshi",
+            profilePic: "images/Funakoshi.jpeg",
+            likes: 4200,
+            retweets: 175,
+            tweetText: "Karate teaches us to use the opponent's strength against them. It's all about leverage and timing.",
+            replies: [
+                {
+                    handle: "@BJJ troll",
+                    profilePic: "images/bjj.jpeg",
+                    tweetText: "Karate is great, but have you tried Brazilian Jiu-Jitsu? Ground game is where it's at! 🤼‍♂️ #BJJ"
+                },
+              {
+                handle: "@Bruce Lee",
+                profilePic: "images/Bruce-Lee.jpg",
+                tweetText: "I'm a fan of all martial arts, but Karate has some awesome techniques! 🥋🤩 #MartialArtsFanatic"
+            },
+            {
+              handle: "@JohnnyLawrence",
+              profilePic: "images/john.jpeg",
+              tweetText: "Shotokahn is impressive, but Cobra Kai is better!"
+            }
+            ],
+            isLiked: false,
+            isRetweeted: false,
+            uuid: "5a6d2f8e-7b3c-4d21-a8e9-1e5c0f9d8b11"
+          },
+]
